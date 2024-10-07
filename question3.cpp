54db919f3401d6ca3b1942b1a489cbeda94cc801
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-07 23:53:10 +0530

    added question3.cpp

---
+++ question3.cpp
@@ -0,0 +1,28 @@
+// Tom and Jerry Chase : 298
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++)
+    {
+        int x;
+        int y;
+        cin>>x>>y;
+        if(x<y)
+        {
+            cout<<"yes"<<endl;
+        }
+        else
+        {
+            cout<<"no"<<endl;
+        }
+    }
+    
+    
+return 0;
+    
+}
