de41e63dde1a066d77b0cde803f8e3c74b2240f4
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-07 23:52:08 +0530

    added question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,20 @@
+// Puzzle Hunt : 279
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+    int n;
+    cin>>n;
+    if (n==6||n==7||n==8){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+    return 0;
+}
