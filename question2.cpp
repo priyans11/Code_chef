36f8520425be23db25e4e95fb956fbc06cd05e3a
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-07 23:52:53 +0530

    added question2.cpp

---
+++ question2.cpp
@@ -0,0 +1,20 @@
+// Chef and Brain Speed : 288
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x,y;
+    cin>>x>>y;
+	// your code goes here
+	if(x<y)
+	{
+	    cout<<"yes";
+	}
+	else 
+	{
+	    cout<<"no";
+	}
+	return 0;
+}
