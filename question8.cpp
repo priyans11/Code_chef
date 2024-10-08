cffd7b8c72f5ac56c549152700ae21674d517115
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-08 23:38:07 +0530

    added question8.cpp

---
+++ question8.cpp
@@ -0,0 +1,16 @@
+// Max minus Min : 339
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        cout<<c-a<<endl;
+    }
+    return 0;
+}
