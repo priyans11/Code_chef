b83a9816044b6a5af65c1068298769e359b7dc3d
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-08 23:36:54 +0530

    added question5.cpp

---
+++ question5.cpp
@@ -0,0 +1,24 @@
+// Capital Gain Tax : 311
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x>y)
+	{
+	    cout<<"decreased";
+	}
+	
+	else if(x<y){
+	    cout<<"INCREASED";
+	}
+	
+	else{
+	    cout<<"same";
+	}
+	
+return 0;
+}
