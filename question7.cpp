4b913c3c4102918eb7eecab8f9d52132194d3872
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-08 23:37:45 +0530

    added question7.cpp

---
+++ question7.cpp
@@ -0,0 +1,19 @@
+// Football Training : 329
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    if(x>y)
+    {
+        cout<<"freekick";
+    }
+    else if(x<y)
+    {
+        cout<<"penalty";
+    }
+    return 0;
+}
