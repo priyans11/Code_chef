ea46c00a3270668b41b41bd8d4b334bf613b1cb8
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-08 23:38:25 +0530

    added question9.cpp

---
+++ question9.cpp
@@ -0,0 +1,16 @@
+// Chef and Chapters : 350
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        cout<<x*y*z<<endl;
+    }
+    return 0;
+}
