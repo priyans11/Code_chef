c2899436936e15c994f18807a2f5a7a6a01038e8
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-09 22:56:38 +0530

    added question13.cpp

---
+++ question13.cpp
@@ -0,0 +1,32 @@
+// The Cheaper Cab : 399
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x<y)
+	    {
+	        cout<<"first"<<endl;
+	    }
+	    else if(x>y)
+	    {
+	        cout<<"second"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"any"<<endl;
+	        
+	        
+	        
+	    }
+	}
+	
+return 0;
+}
