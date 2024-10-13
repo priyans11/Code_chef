2ea6b776a0bd11c8634842fce1687ed9c732ba26
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-13 23:33:15 +0530

    added question19.cpp

---
+++ question19.cpp
@@ -0,0 +1,21 @@
+// Tyre problem : 452
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>>n>>m;
+	    int tyres=2*n+4*m;
+	    
+	    cout<<tyres<<endl;
+	}
+    return 0;
+}
+
