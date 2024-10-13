c6e5994ccdf15c0e0189d5f25763a81cf27e91b7
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-13 23:32:39 +0530

    added question17.cpp

---
+++ question17.cpp
@@ -0,0 +1,25 @@
+// Ezio and Guards : 427
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y;
+	    cin>>x>>y;
+	    if(x>=y){
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	
+    return 0;
+}
