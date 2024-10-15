00bdd2977f18e0669b8839a20fd7f00eea786831
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-15 23:53:08 +0530

    added question26.cpp

---
+++ question26.cpp
@@ -0,0 +1,19 @@
+// Janmansh and Coins : 527
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        cout<<10*x+5*y<<endl;
+    }
+    return 0;
+}
