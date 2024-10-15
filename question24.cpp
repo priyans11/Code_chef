e76247ce9c41807ec3e7ff7116eee6cb81607452
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-15 23:52:30 +0530

    added question24.cpp

---
+++ question24.cpp
@@ -0,0 +1,25 @@
+// Greater Average : 500
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    float d=a+b;
+	    if(d/2>c)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else 
+	    {
+	        cout<<"no"<<endl;
+	    }
+	    
+	}
+    return 0;
+}
