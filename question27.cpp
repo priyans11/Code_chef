250a45e5c9c2f6c0f6b595775ead3315a0ad04cf
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-15 23:53:30 +0530

    added question27.cpp

---
+++ question27.cpp
@@ -0,0 +1,19 @@
+// CRED Coins : 539
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        int z=x*y;
+        cout<<z/100<<endl;
+        
+    }
+}
