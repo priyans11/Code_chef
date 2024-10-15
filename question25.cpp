bd12e0da631b61dea2bbaf9b494d7f91e711916c
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-15 23:52:48 +0530

    added question25.cpp

---
+++ question25.cpp
@@ -0,0 +1,28 @@
+// Literacy Rate : 512
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   float p,l;
+	    cin>>p>>l;
+	   // if()
+	    if(l/p>=0.75)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else 
+	    { 
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+
+}
+
