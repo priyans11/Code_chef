4dede867c9d517d7afca85a512749e32d4503813
priyans11 <144539380+priyans11@users.noreply.github.com>
2024-10-16 23:57:09 +0530

    added question29.cpp

---
+++ question29.cpp
@@ -0,0 +1,24 @@
+// Pizza Cutting : 566
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	   if(n%2==0 || n==1)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+    return 0;
+}
